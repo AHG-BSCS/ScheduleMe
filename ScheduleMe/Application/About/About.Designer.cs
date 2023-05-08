e7641a24282b9a26efc329d0ad28fc974c67297e
VLDZ <ops@vldz.ga>
2023-05-08 22:15:06 +0800

    added scheduleme icon; renamed form1 to appname

--- ScheduleMe/Application/About/About.Designer.cs
+++ ScheduleMe/Application/About/About.Designer.cs
@@ -28,90 +28,92 @@ partial class About
     /// </summary>
     private void InitializeComponent()
     {
-        pictureBox1 = new PictureBox();
-        label1 = new Label();
-        label2 = new Label();
-        label3 = new Label();
-        label4 = new Label();
-        linkLabel1 = new LinkLabel();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-        SuspendLayout();
-        // 
-        // pictureBox1
-        // 
-        pictureBox1.Image = Properties.Resources.scheduleme;
-        pictureBox1.Location = new Point(28, 41);
-        pictureBox1.Name = "pictureBox1";
-        pictureBox1.Size = new Size(164, 161);
-        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-        pictureBox1.TabIndex = 0;
-        pictureBox1.TabStop = false;
-        // 
-        // label1
-        // 
-        label1.AutoSize = true;
-        label1.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
-        label1.Location = new Point(198, 41);
-        label1.Name = "label1";
-        label1.Size = new Size(234, 50);
-        label1.TabIndex = 1;
-        label1.Text = "ScheduleMe";
-        // 
-        // label2
-        // 
-        label2.AutoSize = true;
-        label2.Location = new Point(208, 91);
-        label2.Name = "label2";
-        label2.Size = new Size(80, 20);
-        label2.TabIndex = 2;
-        label2.Text = "Version 1.0";
-        // 
-        // label3
-        // 
-        label3.AutoSize = true;
-        label3.Location = new Point(208, 142);
-        label3.Name = "label3";
-        label3.Size = new Size(253, 20);
-        label3.TabIndex = 3;
-        label3.Text = "Final Project of BS Computer Science";
-        // 
-        // label4
-        // 
-        label4.AutoSize = true;
-        label4.Location = new Point(208, 162);
-        label4.Name = "label4";
-        label4.Size = new Size(213, 20);
-        label4.TabIndex = 4;
-        label4.Text = "Developed By Al Hans Gaming";
-        // 
-        // linkLabel1
-        // 
-        linkLabel1.AutoSize = true;
-        linkLabel1.Location = new Point(212, 215);
-        linkLabel1.Name = "linkLabel1";
-        linkLabel1.Size = new Size(227, 20);
-        linkLabel1.TabIndex = 5;
-        linkLabel1.TabStop = true;
-        linkLabel1.Text = "https://github.com/AHG-BSCS1A";
-        linkLabel1.LinkClicked += linkLabel1_LinkClicked;
-        // 
-        // form_about
-        // 
-        AutoScaleDimensions = new SizeF(8F, 20F);
-        AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(506, 270);
-        Controls.Add(linkLabel1);
-        Controls.Add(label4);
-        Controls.Add(label3);
-        Controls.Add(label2);
-        Controls.Add(label1);
-        Controls.Add(pictureBox1);
-        Name = "form_about";
-        StartPosition = FormStartPosition.CenterParent;
-        Text = "About ScheduleMe";
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-        ResumeLayout(false);
-        PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ScheduleMe.Properties.Resources.scheduleme;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(198, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ScheduleMe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Version 1.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Final Project of BS Computer Science";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(208, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Developed By Al Hans Gaming";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(212, 215);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(227, 20);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/AHG-BSCS1A";
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 270);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "About";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About ScheduleMe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
