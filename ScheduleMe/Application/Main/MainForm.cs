88e2f48bd614efe7a663ee4c3c695eec94e0ce58
Jhondale Llaguno <jhondalellaguno@gmail.com>
2023-05-09 10:23:48 +0800

    initialized all the tab for later use and added the openTabForm

--- ScheduleMe/Application/Main/MainForm.cs
+++ ScheduleMe/Application/Main/MainForm.cs
@@ -4,15 +4,32 @@ namespace ScheduleMe
 {
     public partial class MainForm : Form
     {
+        private Calendar calendar = new Calendar();
+        private Timeline timeline = new Timeline();
+        private Timetable timetable = new Timetable();
+        private Note note = new Note();
+        private Weather weather = new Weather();
+
         public MainForm()
         {
             InitializeComponent();
+            openTabForm(calendar, calendar);
+        }
+
+
+        private void openTabForm(Form activeForm, Form childForm)
+        {
+            activeForm.Hide();
+            childForm.TopLevel = false;
+            childForm.Dock = DockStyle.Fill;
+            tabPanel.Controls.Add(childForm);
+            childForm.Show();
         }
 
 
         private void btnCalendarTab_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnTimelineTab_Click(object sender, EventArgs e)
