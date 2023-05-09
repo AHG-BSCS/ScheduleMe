eff127a0b94c1d2651c5acdb453a8075b0ac24c0
VLDZ <ops@vldz.ga>
2023-05-09 19:38:30 +0800

    added notes function (no sav)

--- ScheduleMe/Application/Note/Note.cs
+++ ScheduleMe/Application/Note/Note.cs
@@ -1,9 +1,77 @@
-﻿namespace ScheduleMe.Tab;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace ScheduleMe.Tab;
 
 public partial class Note : Form
 {
+
+    DataTable notes = new DataTable();
+    bool editing = false;
+    
     public Note()
     {
         InitializeComponent();
+    }
+
+    private void Note_Load(object sender, EventArgs e)
+    {
+        notes.Columns.Add("Title");
+        notes.Columns.Add("Note");
+
+        dataGridView1.DataSource = notes;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        textBox1.Text = notes.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+        textBox2.Text = notes.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+        editing = true;
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+        if (editing)
+        {
+            notes.Rows[dataGridView1.CurrentCell.RowIndex]["Title"] = textBox1.Text;
+            notes.Rows[dataGridView1.CurrentCell.RowIndex]["Note"] = textBox2.Text;
+        }
+        else
+        {
+            notes.Rows.Add(textBox1.Text, textBox2.Text);
+        }
+        textBox1.Text = "";
+        textBox2.Text = "";
+        editing = false;
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+        textBox1.Text = "";
+        textBox2.Text = "";
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            notes.Rows[dataGridView1.CurrentCell.RowIndex].Delete();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+    {
+
+    }
+
+    private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+    {
+        textBox1.Text = notes.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[0].ToString();
+        textBox2.Text = notes.Rows[dataGridView1.CurrentCell.RowIndex].ItemArray[1].ToString();
+        editing = true;
     }
 }
