de7f3cc6dd22de2568c6aee8c3b66359db207861
Jhondale Llaguno <jhondalellaguno@gmail.com>
2023-05-11 18:34:39 +0800

    PopulateEvents parameter is no a List of Tupled startdate and enddate of events. Length of buttons is also now correct.

--- ScheduleMe/Application/Timeline/Timeline.cs
+++ ScheduleMe/Application/Timeline/Timeline.cs
@@ -32,16 +32,20 @@ public partial class Timeline : Form
         }
     }
 
-    private void PopulateEvents(List<DateTime> dates, DateTime startDate)
+    private void PopulateEvents(List<Tuple<DateTime, DateTime>> eventDates, DateTime startDate)
     {
-        foreach (DateTime date in dates)
+        foreach (Tuple<DateTime, DateTime> eventDate in eventDates)
         {
             Button button = new Button();
             button.Text = "Event";
             button.AutoSize = true;
             button.Height = 25;
-            button.Width = 60;
-            button.Location = new Point(panelDateTimeContainer.HorizontalScroll.Value + (date - startDate).Days * 100, 50);
+
+            // Set button width based on event duration
+            int eventDuration = (int)(eventDate.Item2 - eventDate.Item1).TotalDays;
+            button.Width = eventDuration * 100;
+
+            button.Location = new Point(panelDateTimeContainer.HorizontalScroll.Value + (eventDate.Item1 - startDate).Days * 100, 50);
             panelDateTimeContainer.Controls.Add(button);
 
             // Check for overlapping events and stack vertically
@@ -49,8 +53,8 @@ public partial class Timeline : Form
             {
                 if (control is Button && control != button)
                 {
-                    DateTime eventStartDate = date.AddDays(-1);
-                    DateTime eventEndDate = date.AddDays(1);
+                    DateTime eventStartDate = eventDate.Item1.AddDays(-1);
+                    DateTime eventEndDate = eventDate.Item2.AddDays(1);
                     DateTime controlStartDate = startDate.AddDays((control.Location.X - panelDateTimeContainer.HorizontalScroll.Value) / 100);
                     DateTime controlEndDate = startDate.AddDays((control.Location.X - panelDateTimeContainer.HorizontalScroll.Value + control.Width) / 100);
 
@@ -64,23 +68,19 @@ public partial class Timeline : Form
         }
     }
 
-
     private void Timeline_Load(object sender, EventArgs e)
     {
-        List<DateTime> events = new List<DateTime>()
+        List<Tuple<DateTime, DateTime>> events = new List<Tuple<DateTime, DateTime>>()
         {
-            new DateTime(2023, 4, 13, 0, 0, 0),
-            new DateTime(2023, 4, 21, 0, 0, 0),
-            new DateTime(2023, 4, 27, 0, 0, 0),
-            new DateTime(2023, 4, 12, 0, 0, 0),
-            new DateTime(2023, 4, 12, 0, 0, 0),
-            new DateTime(2023, 4, 12, 0, 0, 0),
-            new DateTime(2023, 4, 16, 0, 0, 0),
-            new DateTime(2023, 5, 1, 0, 0, 0),
-            new DateTime(2023, 5, 2, 0, 0, 0)
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 12), new DateTime(2023, 4, 13)),
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 13), new DateTime(2023, 4, 15)),
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 15), new DateTime(2023, 4, 18)),
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 17), new DateTime(2023, 4, 21)),
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 22), new DateTime(2023, 4, 23)),
+            new Tuple<DateTime, DateTime>(new DateTime(2023, 4, 12), new DateTime(2023, 5, 1)),
         };
 
-        PopulateDates(new DateTime(2023, 4, 12, 0, 0, 0), new DateTime(2023, 5, 16, 23, 59, 59));
-        PopulateEvents(events, new DateTime(2023, 4, 12, 0, 0, 0));
+        PopulateDates(new DateTime(2023, 4, 12), new DateTime(2023, 5, 16));
+        PopulateEvents(events, new DateTime(2023, 4, 12));
     }
 }
