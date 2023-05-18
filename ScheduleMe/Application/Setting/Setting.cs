328c49ac4465652c7503f72f7119b68dcb4b508a
VLDZ <ops@vldz.ga>
2023-05-18 18:26:45 +0800

    added logic of dark mode

--- ScheduleMe/Application/Setting/Setting.cs
+++ ScheduleMe/Application/Setting/Setting.cs
@@ -13,4 +13,16 @@ public partial class Setting : Form
     {
         FormClosedEvent?.Invoke(this, e);
     }
+
+    private void checkBox1_CheckedChanged(object sender, EventArgs e)
+    {
+        if (checkBox1.Checked == true)
+        {
+            this.BackColor = Color.DarkGray;
+        }
+        else
+        {
+            this.BackColor = Color.White;
+        }
+    }
 }
