6b4f0eba8e4fc0acd61f054b36c881df09469d38
VLDZ <ops@vldz.ga>
2023-05-23 20:41:50 +0800

    added initial logic for weather tab

--- ScheduleMe/Application/Weather/Weather.Designer.cs
+++ ScheduleMe/Application/Weather/Weather.Designer.cs
@@ -28,36 +28,75 @@ partial class Weather
     /// </summary>
     private void InitializeComponent()
     {
-        label1 = new Label();
-        SuspendLayout();
-        // 
-        // label1
-        // 
-        label1.AutoSize = true;
-        label1.Location = new Point(12, 13);
-        label1.Name = "label1";
-        label1.Size = new Size(57, 22);
-        label1.TabIndex = 1;
-        label1.Text = "Weather";
-        // 
-        // Weather
-        // 
-        AutoScaleDimensions = new SizeF(96F, 96F);
-        AutoScaleMode = AutoScaleMode.Dpi;
-        BackColor = Color.White;
-        ClientSize = new Size(850, 550);
-        Controls.Add(label1);
-        Font = new Font("Dubai", 9.749999F, FontStyle.Regular, GraphicsUnit.Point);
-        FormBorderStyle = FormBorderStyle.None;
-        Margin = new Padding(3, 4, 3, 4);
-        Name = "Weather";
-        Text = "Weather";
-        WindowState = FormWindowState.Maximized;
-        ResumeLayout(false);
-        PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Weather";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(360, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 120);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CONDITION";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CITY";
+            // 
+            // Weather
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Dubai", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Weather";
+            this.Text = "Weather";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Weather_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
 
     private Label label1;
+    private PictureBox pictureBox1;
+    private Label label2;
+    private Label label3;
 }
--- ScheduleMe/Application/Weather/Weather.cs
+++ ScheduleMe/Application/Weather/Weather.cs
@@ -1,9 +1,24 @@
-﻿namespace ScheduleMe.Tab;
+﻿using System.Net;
+
+namespace ScheduleMe.Tab;
 
 public partial class Weather : Form
 {
     public Weather()
     {
         InitializeComponent();
+    }
+
+    private async void Weather_Load(object sender, EventArgs e)
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("https://api.weatherapi.com/v1/current.json?key=168c66e6792c4e7dbe5122918232305&q=San+Pablo+Laguna&aqi=no");
+        HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+        string result = await response.Content.ReadAsStringAsync();
+        dynamic post = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
+
+        pictureBox1.Image = new Bitmap(new MemoryStream(new WebClient().DownloadData("https:" + post.current.condition.icon)));
+        label2.Text = post.current.condition.text;
+        label3.Text = post.location.name + ", " + post.location.region;
     }
 }
