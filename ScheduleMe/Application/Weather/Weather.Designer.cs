a730887630d4d3ab196d255d87352dfb0007828c
VLDZ <ops@vldz.ga>
2023-05-24 09:27:03 +0800

    added network checks for weather and timetable tabs

--- ScheduleMe/Application/Weather/Weather.Designer.cs
+++ ScheduleMe/Application/Weather/Weather.Designer.cs
@@ -34,7 +34,11 @@ partial class Weather
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -48,53 +52,89 @@ partial class Weather
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(360, 124);
+            this.pictureBox1.Location = new System.Drawing.Point(60, 186);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(125, 120);
+            this.pictureBox1.Size = new System.Drawing.Size(65, 61);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(394, 256);
+            this.label2.Font = new System.Drawing.Font("Dubai Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(159, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 29);
+            this.label2.Size = new System.Drawing.Size(111, 29);
             this.label2.TabIndex = 3;
             this.label2.Text = "CONDITION";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(394, 74);
+            this.label3.Font = new System.Drawing.Font("Dubai", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(666, 62);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 29);
+            this.label3.Size = new System.Drawing.Size(168, 102);
             this.label3.TabIndex = 4;
             this.label3.Text = "CITY";
+            this.label3.TextChanged += new System.EventHandler(this.label3_TextChanged);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(589, 209);
+            this.textBox1.Location = new System.Drawing.Point(1237, 387);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(182, 35);
             this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "San Pablo Laguna";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(632, 256);
+            this.button1.Location = new System.Drawing.Point(1283, 450);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(94, 45);
             this.button1.TabIndex = 6;
             this.button1.Text = "SEND";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Dubai Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(60, 600);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "TODAY\'S FORECAST:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(60, 326);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(65, 61);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Dubai Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(159, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CHANCE OF RAIN";
             // 
             // Weather
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.ClientSize = new System.Drawing.Size(1565, 935);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
@@ -109,6 +149,7 @@ partial class Weather
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Weather_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,4 +163,7 @@ partial class Weather
     private Label label3;
     private TextBox textBox1;
     private Button button1;
+    private Label label4;
+    private PictureBox pictureBox2;
+    private Label label5;
 }
--- ScheduleMe/Application/Weather/Weather.cs
+++ ScheduleMe/Application/Weather/Weather.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Security.Policy;
 using static System.Net.WebRequestMethods;
@@ -12,15 +13,52 @@ public partial class Weather : Form
         InitializeComponent();
     }
 
+    private bool IsInternetConnected()
+    {
+        try
+        {
+            using (Ping ping = new Ping())
+            {
+                PingReply reply = ping.Send("api.weatherapi.com", 1000); // Replace with a reliable host address
+                return (reply != null && reply.Status == IPStatus.Success);
+            }
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
     private async void Weather_Load(object sender, EventArgs e)
     {
+        HttpClient client = new HttpClient();
+        var url_calix = "https://api.weatherapi.com/v1/forecast.json?key=168c66e6792c4e7dbe5122918232305&q=" + textBox1.Text + "&days=1&aqi=no";
+        client.BaseAddress = new Uri(url_calix);
+        if (IsInternetConnected() == true)
+        {
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+            string result = await response.Content.ReadAsStringAsync();
+            dynamic post = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
 
+            pictureBox1.Image = new Bitmap(new MemoryStream(new WebClient().DownloadData("https:" + post.current.condition.icon)));
+            label2.Text = post.current.condition.text;
+            label3.Text = post.location.name + ", " + post.location.region;
+
+            //forecast
+
+            pictureBox2.Image = new Bitmap(new MemoryStream(new WebClient().DownloadData("https:" + post.forecast.forecastday[0].day.condition.icon)));
+            label5.Text = "Expect " + post.forecast.forecastday[0].day.condition.text + " with a " + post.forecast.forecastday[0].day.daily_chance_of_rain + "%" + " chance of rain";
+        }
+        else
+        {
+            MessageBox.Show("Cannot reach Weather API, please check your network connection.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 
     private async void button1_Click(object sender, EventArgs e)
     {
         HttpClient client = new HttpClient();
-        var url_calix = "https://api.weatherapi.com/v1/current.json?key=168c66e6792c4e7dbe5122918232305&q=" + textBox1.Text + "&aqi=no";
+        var url_calix = "https://api.weatherapi.com/v1/forecast.json?key=168c66e6792c4e7dbe5122918232305&q=" + textBox1.Text + "&aqi=no";
         client.BaseAddress = new Uri(url_calix);
         HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
         string result = await response.Content.ReadAsStringAsync();
@@ -29,5 +67,20 @@ public partial class Weather : Form
         pictureBox1.Image = new Bitmap(new MemoryStream(new WebClient().DownloadData("https:" + post.current.condition.icon)));
         label2.Text = post.current.condition.text;
         label3.Text = post.location.name + ", " + post.location.region;
+
+        //forecast
+
+        pictureBox2.Image = new Bitmap(new MemoryStream(new WebClient().DownloadData("https:" + post.forecast.forecastday[0].day.condition.icon)));
+        label5.Text = "Expect " + post.forecast.forecastday[0].day.condition.text + " with a " + post.forecast.forecastday[0].day.daily_chance_of_rain + "%" + " chance of rain";
+    }
+    
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+        this.AcceptButton = button1;
+    }
+
+    private void label3_TextChanged(object sender, EventArgs e)
+    {
+        this.CenterToParent();
     }
 }
