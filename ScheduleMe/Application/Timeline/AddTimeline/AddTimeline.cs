ef29ff098f67d45519f9728c8d83ad97ec66012b
Jhondale Llaguno <jhondalellaguno@gmail.com>
2023-05-25 10:56:37 +0800

    Adding a timeline now prevents wrong inputs of data and promt a message for specific error

--- ScheduleMe/Application/Timeline/AddTimeline/AddTimeline.cs
+++ ScheduleMe/Application/Timeline/AddTimeline/AddTimeline.cs
@@ -13,6 +13,24 @@ public partial class AddTimeline : Form
 
     private void btnSaveTimeline_Click(object sender, EventArgs e)
     {
+        if (tBoxName.Text != "")
+        {
+            if (startDatePicker.Value < endDatePicker.Value)
+                CreateTimeline();
+            else
+                MessageBox.Show("Invalid Dates");
+        }
+        else
+            MessageBox.Show("Invalid Name");
+    }
+
+    private void btnCancelTimeline_Click(object sender, EventArgs e)
+    {
+        this.Close();
+    }
+
+    private void CreateTimeline()
+    {
         using (var timelineDB = new LiteDatabase(DBConnection.timelineConnection))
         {
             var timelines = timelineDB.GetCollection<Timeline>("Timeline");
@@ -25,32 +43,27 @@ public partial class AddTimeline : Form
             };
 
             // For testing purpose only. To be remove later
-            if (tBoxName.Text == "Test")
+            if (tBoxName.Text == "Test Dates")
             {
                 newTimeline.Events = new List<Event>
                 {
-                    new Event { EventStartDate = new DateTime(2023, 4, 6), EventEndDate = new DateTime(2023, 4, 9) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 4), EventEndDate = new DateTime(2023, 4, 8) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 5), EventEndDate = new DateTime(2023, 4, 7) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 2), EventEndDate = new DateTime(2023, 4, 4) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 3), EventEndDate = new DateTime(2023, 4, 6) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 5) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 2) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 3) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 6), EventEndDate = new DateTime(2023, 4, 16) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 11), EventEndDate = new DateTime(2023, 4, 17) },
-                    new Event { EventStartDate = new DateTime(2023, 4, 12), EventEndDate = new DateTime(2023, 4, 16) },
-                    new Event { EventStartDate = new DateTime(2023, 5, 18), EventEndDate = new DateTime(2023, 5, 21) }
+                    new Event { EventStartDate = new DateTime(2023, 4, 6), EventEndDate = new DateTime(2023, 4, 9), EventColor = -15987921, EventTitle = "Event 1" },
+                    new Event { EventStartDate = new DateTime(2023, 4, 4), EventEndDate = new DateTime(2023, 4, 8), EventColor = -15987921, EventTitle = "Event 2" },
+                    new Event { EventStartDate = new DateTime(2023, 4, 5), EventEndDate = new DateTime(2023, 4, 7), EventColor = -15987921, EventTitle = "Event 3" },
+                    new Event { EventStartDate = new DateTime(2023, 4, 2), EventEndDate = new DateTime(2023, 4, 4), EventColor = -15987921, EventTitle = "Event 4" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 3), EventEndDate = new DateTime(2023, 4, 6), EventColor = -15987921, EventTitle = "Event 5" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 5), EventColor = -15987921, EventTitle = "Event 6" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 2), EventColor = -15987921, EventTitle = "Event 7" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 1), EventEndDate = new DateTime(2023, 4, 3), EventColor = -15987921, EventTitle = "Event 8" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 6), EventEndDate = new DateTime(2023, 4, 16), EventColor = -15987921, EventTitle = "Event 9" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 11), EventEndDate = new DateTime(2023, 4, 17), EventColor = -15987921, EventTitle = "Event 10" },
+                    new Event {EventStartDate = new DateTime(2023, 4, 12), EventEndDate = new DateTime(2023, 4, 16), EventColor = -15987921, EventTitle = "Event 11" },
+                    new Event { EventStartDate = new DateTime(2023, 5, 18), EventEndDate = new DateTime(2023, 5, 21), EventColor = -15987921, EventTitle = "Event 12" }
                 };
             }
             timelines.Insert(newTimeline);
             Id = newTimeline.Id;
         }
-        this.Close();
-    }
-
-    private void btnCancelTimeline_Click(object sender, EventArgs e)
-    {
         this.Close();
     }
 }
